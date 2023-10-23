af83047bc63b8e6dfa726959a3a72a94acc59429
Eduardo <amgp@ymail.com>
2023-10-23 16:15:32 -0300

    Criado Outras entidades Segunda Migration

--- VendasWebMvc/Models/Departament.cs
+++ VendasWebMvc/Models/Departament.cs
@@ -9,5 +9,27 @@ namespace VendasWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Vendedor> Vendedores { get; set; } = new List<Vendedor>();
+
+        public Departament()
+        {
+
+        }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddVendedores(Vendedor vendedor)
+        {
+            Vendedores.Add(vendedor);
+        }
+
+        public double TotalVendas(DateTime initial, DateTime final)
+        {
+            return Vendedores.Sum(vendedor => vendedor.TotalVendas(initial, final));
+        }
     }
 }
